a7782df30df5124c9f646c3ca6b132c76d179009
Rina-01 <ira.mashkova.2001@mail.ru>
2023-12-15 23:01:03 +0300

    new names

--- college/polls/models.py
+++ college/polls/models.py
@@ -1,41 +1,67 @@
 from django.db import models
 
 
-
 class Student(models.Model):
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
     second_name = models.CharField(max_length=128, blank=True)
     date_of_birth = models.DateField()
 
-class Groups(models.Model):
+    class Meta:
+        db_table = 'student'
+
+
+class Group(models.Model):
     name = models.CharField(max_length=16)
 
-class Disciplines(models.Model):
+    class Meta:
+        db_table = 'group'
+
+
+class Discipline(models.Model):
     name = models.CharField(max_length=128)
     description = models.CharField(max_length=1000, blank=True)
 
-class Tutors(models.Model):
+    class Meta:
+        db_table = 'discipline'
+
+
+class Tutor(models.Model):
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
     second_name = models.CharField(max_length=128, blank=True)
     date_of_birth = models.DateField()
 
+    class Meta:
+        db_table = 'tutor'
 
-class Group_semesters(models.Model):
+
+class GroupSemester(models.Model):
     semester_num = models.PositiveSmallIntegerField()
-    group = models.ForeignKey(Groups, on_delete=models.PROTECT)
+    group = models.ForeignKey(Group, on_delete=models.PROTECT)
 
-class Group_members(models.Model):
-    group_semesters = models.ForeignKey(Group_semesters, on_delete=models.PROTECT)
+    class Meta:
+        db_table = 'group_semester'
+
+
+class GroupMember(models.Model):
+    group_semesters = models.ForeignKey(GroupSemester, on_delete=models.PROTECT)
     student = models.ForeignKey(Student, on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = 'group_member'
+
 
 class Curriculum(models.Model):
     duration = models.PositiveSmallIntegerField()
-    discipline = models.ForeignKey(Disciplines, on_delete=models.PROTECT)
-    group_semester = models.ForeignKey(Group_semesters, on_delete=models.PROTECT)
+    discipline = models.ForeignKey(Discipline, on_delete=models.PROTECT)
+    group_semester = models.ForeignKey(GroupSemester, on_delete=models.PROTECT)
 
-class Final_grades(models.Model):
+    class Meta:
+        db_table = 'curriculum'
+
+
+class FinalGrade(models.Model):
     # semester_num = models.PositiveSmallIntegerField(blank=True)
     is_final = models.BooleanField()
     scale_5 = models.PositiveSmallIntegerField()
@@ -45,10 +71,18 @@ class Final_grades(models.Model):
     student = models.ForeignKey(Student, on_delete=models.PROTECT)
     curriculum = models.ForeignKey(Curriculum, on_delete=models.PROTECT)
 
-class Curriculum_lesson(models.Model):
+    class Meta:
+        db_table = 'finalGrade'
+
+
+class CurriculumLesson(models.Model):
     lesson_type = models.PositiveSmallIntegerField()
     curriculum = models.ForeignKey(Curriculum, on_delete=models.PROTECT)
-    tutor = models.ForeignKey(Tutors, on_delete=models.PROTECT)
+    tutor = models.ForeignKey(Tutor, on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = 'curriculum_Lesson'
+
 
 class LessonTime(models.Model):
     start_time = models.TimeField()
@@ -57,11 +91,13 @@ class LessonTime(models.Model):
     class Meta:
         db_table = 'lessons_time'
 
+
 class Classroom(models.Model):
     number = models.CharField(max_length=8)
 
     class Meta:
-        db_table = 'classrooms'
+        db_table = 'classroom'
+
 
 class TTLesson(models.Model):
     date = models.DateField()
@@ -69,10 +105,12 @@ class TTLesson(models.Model):
     week_type = models.PositiveSmallIntegerField()
     lessons_time_id = models.ForeignKey(LessonTime, on_delete=models.PROTECT)
     classroom_id = models.ForeignKey(Classroom, on_delete=models.PROTECT)
-    curriculum_lesson_id = models.ForeignKey(Curriculum_lesson, on_delete=models.PROTECT)
+    curriculum_lesson_id = models.ForeignKey(CurriculumLesson, on_delete=models.PROTECT)
 
     class Meta:
         db_table = 'tt_lesson'
+
+
 class Homework(models.Model):
     description = models.CharField(max_length=2048)
     day_given = models.ForeignKey(TTLesson, on_delete=models.PROTECT)
@@ -81,16 +119,18 @@ class Homework(models.Model):
     student_id = models.ForeignKey(Student, on_delete=models.PROTECT)
 
     class Meta:
-        db_table = 'homeworks'
+        db_table = 'homework'
 
-class Files(models.Model):
+
+class File(models.Model):
     name = models.CharField(max_length=64)
     description = models.CharField(max_length=2048, blank=True)
     file = models.BinaryField()
     hw_id = models.ForeignKey(Homework, on_delete=models.PROTECT)
 
     class Meta:
-        db_table = 'files'
+        db_table = 'file'
+
 
 class StudentAttendance(models.Model):
     is_present = models.BooleanField()
@@ -100,6 +140,7 @@ class StudentAttendance(models.Model):
     class Meta:
         db_table = 'students_attendance'
 
+
 class StudentProgress(models.Model):
     tt_lesson_id = models.ForeignKey(TTLesson, on_delete=models.PROTECT)
     student_id = models.ForeignKey(Student, on_delete=models.PROTECT)
@@ -107,14 +148,16 @@ class StudentProgress(models.Model):
     class Meta:
         db_table = 'students_progress'
 
+
 class Coefficient(models.Model):
     coef_num = models.PositiveSmallIntegerField()
     description = models.CharField(max_length=64)
 
     class Meta:
-        db_table = 'coefficients'
+        db_table = 'coefficient'
 
-class Grades(models.Model):
+
+class Grade(models.Model):
     scale_5 = models.PositiveSmallIntegerField()
     scale_word = models.CharField(max_length=32)
     scale_100 = models.PositiveSmallIntegerField()
@@ -125,4 +168,4 @@ class Grades(models.Model):
     progress_id = models.ForeignKey(StudentProgress, on_delete=models.PROTECT)
 
     class Meta:
-        db_table = 'grades'
+        db_table = 'grade'
